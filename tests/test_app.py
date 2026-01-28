011173aee42201e01df20add11073c9c96877b4b
edvaniocorreiacorreia@gmail.com <edvaniocorreiacorreia@gmail.com>
2026-01-28 14:22:35 +0100

    Multiply function adn CI workflow

--- tests/test_app.py
+++ tests/test_app.py
@@ -27,5 +27,5 @@ class TestMultiply:
     """Tests for multiply function."""
     def test_multiply(self):
         assert multiply(3, 4) == 12
-        assert multiply(0, 5) == 2
+        assert multiply(0, 5) == 0
         assert multiply(-2, 3) == -6
